a18737af1ce64b5e219db899d0887be14d0c8612
Oleksii Markov <alex@markovs.me>
2017-10-25 14:10:13 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,15 @@ cwd = path.abspath(path.dirname(__file__))
 with open(path.join(cwd, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(cwd, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().split()
+
 setup(
     name='importable',
-    version='0.1.0',
+    version='0.2.0',
 
-    description='Allows to import zip-compressed Python package by URL.',
+    description=(
+        'Allows to import zip-compressed Python package by URL (http, hdfs).'),
     long_description=long_description,
     url='https://github.com/malexer/importable',
 
@@ -31,10 +35,8 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 
-    keywords=('import url importable python_path'),
+    keywords=('import url http hdfs zip importable python_path'),
 
     py_modules=['importable'],
-    install_requires=[
-        'requests',
-    ],
+    install_requires=requirements,
 )
